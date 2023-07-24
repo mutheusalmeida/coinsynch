3bd16ec4d810bad016c4c4830b3e0924bcdf4770
Matheus Almeida <matheusalmeida016@gmail.com>
2023-07-24 11:11:39 -0400

    feat: add site menu

--- src/components/Router/Router.tsx
+++ src/components/Router/Router.tsx
@@ -1,17 +1,17 @@
+import { DashboardLayout } from '@/layouts/DashboardLayout'
 import {
   Route,
   createBrowserRouter,
   createRoutesFromElements,
 } from 'react-router-dom'
-import { DashboardLayout } from '../../layouts/DashboardLayout'
 import { Analytics } from '../Analytics'
 import { Dashboard } from '../Dashboard'
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/' element={<DashboardLayout />}>
+    <Route path="/" element={<DashboardLayout />}>
       <Route index element={<Dashboard />} />
-      <Route path='analytics' element={<Analytics />} />
+      <Route path="analytics" element={<Analytics />} />
     </Route>
   ),
   { basename: '/dashboard' }
