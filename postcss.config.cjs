c2206c78ffaceb26d00cd9fc11c14c2b42642d64
Matheus Almeida <matheusalmeida016@gmail.com>
2023-07-31 10:31:31 -0400

    improve: update postcss config

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -3,5 +3,7 @@ module.exports = {
     require('postcss-preset-env')({
       stage: 0,
     }),
+    require('tailwindcss/nesting')(require('postcss-nesting')),
+    require('tailwindcss'),
   ],
 }
